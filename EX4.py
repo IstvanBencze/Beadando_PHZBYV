c993afaf453083dd47bc9e7d8f67c40fbb6acc59
IstvanBencze <37245223+IstvanBencze@users.noreply.github.com>
2018-04-29 23:44:22 +0200

    Update EX4.py

--- EX4.py
+++ EX4.py
@@ -1,2 +1,10 @@
 def fordito(mondat):
-    
+    PigLatin=""
+    for i in range(len(mondat)):
+        if mondat[i]==" ":
+            PigLatin+="ay "
+        else:
+            PigLatin+=mondat[i]
+    PigLatin+="ay"
+    return PigLatin
+print(fordito("The quick brown fox"))
