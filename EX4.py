5ff53babd9819721bf3ce2201d4bccc1e904196e
IstvanBencze <37245223+IstvanBencze@users.noreply.github.com>
2018-04-30 13:24:29 +0200

    Update EX4.py

--- EX4.py
+++ EX4.py
@@ -1,6 +1,8 @@
 def fordito(mondat):
     PigLatin=""
     szavak=mondat.split(" ")
+    szavak[0]=szavak[0].replace(szavak[0],szavak[0].lower())
+    szavak[0]=szavak[0].replace(szavak[0][1],szavak[0][1].upper())
     for i in range(len(szavak)):
         PigLatin+=szavak[i]+szavak[i][0]+"ay "
     return PigLatin
