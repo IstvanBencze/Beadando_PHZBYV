26635c57bcc49a38ba9a9a36c43c67e688aab41e
IstvanBencze <37245223+IstvanBencze@users.noreply.github.com>
2018-04-30 12:55:41 +0200

    Update EX4.py

--- EX4.py
+++ EX4.py
@@ -1,15 +1,7 @@
 def fordito(mondat):
     PigLatin=""
-    tmp=""
-    for i in range(len(mondat)):
-        if i==0:
-            tmp=mondat[0]
-        else:
-            if mondat[i]==" ":
-                PigLatin+=tmp+"ay "
-                tmp=mondat[i+1]
-            else:
-                PigLatin+=mondat[i]
-    PigLatin+="ay"
+    szavak=mondat.split(" ")
+    for i in range(len(szavak)):
+        PigLatin+=szavak[i]+szavak[i][0]+"ay "
     return PigLatin
 print(fordito("The quick brown fox"))
