f2bb7d9c71f5eae778e78db48ccbdc775539e075
IstvanBencze <37245223+IstvanBencze@users.noreply.github.com>
2018-05-01 23:54:26 +0200

    Update EX10.py

--- EX10.py
+++ EX10.py
@@ -1,1 +1,23 @@
 def legkisebb():
+    i=10
+    while True:
+        if i%19==0:
+            if i%18==0:
+                if i%17==0:
+                    if i%16==0:
+                        if i%15==0:
+                            if i%14==0:
+                                if i%13==0:
+                                    if i%12==0:
+                                        if i%11==0:
+                                            if i%9==0:
+                                                if i%8==0:
+                                                    if i%7==0:
+                                                        if i%6==0:
+                                                            if i%5==0:
+                                                                if i%4==0:
+                                                                    if i%3==0:
+                                                                        if i%2==0:
+                                                                            return i
+        i+=10
+print(legkisebb())
