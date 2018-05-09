6db6ecad70b40d864f410dc58e054a076d3b8461
IstvanBencze <37245223+IstvanBencze@users.noreply.github.com>
2018-05-09 17:36:44 +0200

    Update EX4.py

--- EX4.py
+++ EX4.py
@@ -1,10 +1,21 @@
-def fordito(mondat):
-    PigLatin=""
-    szavak=mondat.split(" ")
-    szavak[0]=szavak[0].replace(szavak[0],szavak[0].lower())
-    szavak[0]=szavak[0].replace(szavak[0][1],szavak[0][1].upper())
-    for i in range(len(szavak)):
-        PigLatin+=szavak[i][1:]+szavak[i][0]+"ay "
-    PigLatin=PigLatin[0:-1]
-    return PigLatin
-print(fordito("The quick brown fox"))
+def fordito():
+    fordit = input("Hogyan szeretne forditani?\n(PigLatinrol vagy Piglatinra)\n")
+    mondat = input("Adjon meg egy mondatot: ")
+    szavak = mondat.split(" ")
+    if fordit=="PigLatinra":
+        PigLatin = ""
+        szavak[0]=szavak[0].replace(szavak[0],szavak[0].lower())
+        szavak[0]=szavak[0].replace(szavak[0][1],szavak[0][1].upper())
+
+        for i in range(len(szavak)):
+            PigLatin+=szavak[i][1:]+szavak[i][0]+"ay "
+        PigLatin=PigLatin[0:-1]
+        return PigLatin
+    elif fordit=="PigLatinrol":
+        s=""
+        szavak[0] = szavak[0].replace(szavak[0], szavak[0].lower())
+        szavak[0] = szavak[0].replace(szavak[0][-3], szavak[0][-3].upper())
+        for i in range(len(szavak)):
+            s+=szavak[i][-3]+szavak[i][:-3]+" "
+        return s
+print(fordito())
