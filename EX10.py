afeeb6e9f7f8ea267e99319910c4aa986ce4cf35
IstvanBencze <37245223+IstvanBencze@users.noreply.github.com>
2018-05-09 16:10:31 +0200

    Update EX10.py

--- EX10.py
+++ EX10.py
@@ -3,5 +3,21 @@ def legkisebb():
     while True:
         if i%19==0 and i%18==0 and i%17==0 and i%16==0 and i%15==0 and i%14==0 and i%13==0 and i%12==0 and i%11==0:
             return i
-        i+=20
+        i+=2520
+print(legkisebb())
+
+def legkisebb():
+    i=2520
+    flag=0
+    while True:
+        for j in range(11,20):
+            if i%j==0:
+                flag=1
+            else:
+                flag=0
+                break
+        if flag==1:
+            return i
+
+        i+=2520
 print(legkisebb())
